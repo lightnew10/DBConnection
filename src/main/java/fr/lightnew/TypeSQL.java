4cfebffd7e6063f3bd44722df0cba84dd823e7c6
lightnew <louiis.hus@gmail.com>
2024-09-28 20:28:11 +0200

    Update TypeSQL.java
    set default value

--- src/main/java/fr/lightnew/TypeSQL.java
+++ src/main/java/fr/lightnew/TypeSQL.java
@@ -1,33 +1,40 @@
 package fr.lightnew;
 
+import lombok.Getter;
+
+@Getter
 public enum TypeSQL {
-    TINYINT,
-    SMALLINT,
-    MEDIUMINT,
-    INT,
-    BIGINT,
-    FLOAT,
-    DOUBLE,
-    DECIMAL,
-    DATE,
-    DATETIME,
-    TIMESTAMP,
-    TIME,
-    YEAR,
-    CHAR,
-    VARCHAR,
-    TINYTEXT,
-    TEXT,
-    MEDIUMTEXT,
-    LONGTEXT,
-    BINARY,
-    VARBINARY,
-    TINYBLOB,
-    BLOB,
-    MEDIUMBLOB,
-    LONGBLOB,
-    ENUM,
-    SET,
-    JSON,
-    GEOMETRY;
+    TINYINT("TINYINT"),
+    SMALLINT("SMALLINT"),
+    MEDIUMINT("MEDIUMINT"),
+    INT("INT"),
+    BIGINT("BIGINT"),
+    FLOAT("FLOAT"),
+    DOUBLE("DOUBLE"),
+    DECIMAL("DECIMAL(10,2)"),
+    DATE("DATE"),
+    DATETIME("DATETIME"),
+    TIMESTAMP("TIMESTAMP"),
+    TIME("TIME"),
+    YEAR("YEAR"),
+    CHAR("CHAR(100)"),
+    VARCHAR("VARCHAR(255)"),
+    TINYTEXT("TINYTEXT"),
+    TEXT("TEXT"),
+    MEDIUMTEXT("MEDIUMTEXT"),
+    LONGTEXT("LONGTEXT"),
+    BINARY("BINARY(16)"),
+    VARBINARY("VARBINARY(255)"),
+    TINYBLOB("TINYBLOB"),
+    BLOB("BLOB"),
+    MEDIUMBLOB("MEDIUMBLOB"),
+    LONGBLOB("LONGBLOB"),
+    JSON("JSON"),
+    GEOMETRY("GEOMETRY");
+
+    private String type;
+
+    TypeSQL(String type) {
+        this.type = type;
+    }
 }
